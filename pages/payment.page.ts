6a3ac364c8430918bb2a2539be4a846555b3fa55
Martyna Kowalczyk <157369413+BioMartinez@users.noreply.github.com>
2024-06-28 13:17:13 +0200

    Add side-menu component

--- pages/payment.page.ts
+++ pages/payment.page.ts
@@ -1,8 +1,11 @@
 import { Page } from '@playwright/test';
+import { SideMenuComponent } from '../components/side-menu.components';
 
 export class PaymentPage {
   constructor(private page: Page) {}
 
+  sideMenu = new SideMenuComponent(this.page);
+  
   transferReceiver = this.page.getByTestId('transfer_receiver');
   transferReceiverAccountNumber = this.page.getByTestId('form_account_to');
   transferAmount = this.page.getByTestId('form_amount');
